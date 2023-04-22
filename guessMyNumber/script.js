2df33322872b7752db79203195608c12cef39d2d
Sezer <limanlarsezer@gmail.com>
2023-04-22 14:45:55 +0300

    comment lines updated

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -1,4 +1,5 @@
 'use strict';
+//DOM Manipülasyon işlemleri
 const btnAgain = document.querySelector('.again');
 const btnCheck = document.querySelector('.check');
 const guess = document.querySelector('.guess');
@@ -10,54 +11,62 @@ const body = document.querySelector('body');
 const scoreArray = [];
 
 let scores = 20;
-
+//1 - 20 arası random sayı
 let rndNumber = Math.floor(Math.random() * 20) + 1;
 
-console.log(rndNumber); //
+console.log(rndNumber); //test
+
+//Tıklama eventleri
 btnCheck.addEventListener('click', () => {
+  //Girilen sayı random sayıdan küçükse;
   if (rndNumber < guess.value) {
-    msg.innerHTML = 'Too High!';
+    msg.textContent = 'Too High!';
     scores--;
-    score.innerHTML = scores;
+    score.textContent = scores;
+    //Girilen sayı random sayıdan büyükse;
   } else if (rndNumber > guess.value) {
-    msg.innerHTML = 'Too Low!';
+    msg.textContent = 'Too Low!';
 
     scores--;
-    score.innerHTML = scores;
+    score.textContent = scores;
+    //Girilen sayı random sayıya eşitse;
   } else if (rndNumber == guess.value) {
-    msg.innerHTML = 'Correct Number!';
+    msg.textContent = 'Correct Number!';
     body.style.backgroundColor = 'green';
-
     scores--;
     btnCheck.disabled = true;
-    score.innerHTML = scores;
-    number.innerHTML = rndNumber;
+    score.textContent = scores;
+    number.textContent = rndNumber;
     scoreArray.push(scores);
-    highscore.innerHTML = scoreArray.reduce((a, b) => {
+
+    highscore.textContent = scoreArray.reduce((a, b) => {
+      //Math.max(...scoreArray) kullanılabilir.
       return b > a ? b : a;
     }, 0);
     console.log(scoreArray);
   }
-
+  //Score 0'a eşitse oyun sonlanır.
   if (scores == 0) {
     btnCheck.disabled = true;
     btnCheck.style.cursor = 'no-drop';
-    msg.innerHTML = 'GAMEOVER!';
+    msg.textContent = 'GAMEOVER!';
   }
 });
-
+//Oyunu tekrar başlatmak için resetGame fonksiyonu.
 const resetGame = function () {
   rndNumber = Math.floor(Math.random() * 20) + 1;
   guess.value = '';
-  msg.innerHTML = 'Start guessing...';
-  number.innerHTML = '?';
+  msg.textContent = 'Start guessing...';
+  number.textContent = '?';
   scores = 20;
-  score.innerHTML = scores;
+  score.textContent = scores;
   body.style.backgroundColor = '#222';
-  highscore.innerHTML = scoreArray.reduce((a, b) => {
+  highscore.textContent = scoreArray.reduce((a, b) => {
+    //Math.max(...scoreArray) kullanılabilir.
     return b > a ? b : a;
   }, 0);
   btnCheck.disabled = false;
   btnCheck.style.cursor = 'pointer';
 };
+//Tıklama eventi ile oyunu sıfırlama fonksiyonunu çağırma
 btnAgain.addEventListener('click', resetGame);
